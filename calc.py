3f4b858f3feaacf5eeaad82cc4ceb79c5ab7cd2a
jim-ayushi-uw <chayushi007@gmail.com>
2024-06-14 18:30:16 +0530

    projects

---
+++ calc.py
@@ -0,0 +1,25 @@
+print("The Love Calculator is calculating your score...")
+name1 = input()  # What is your name?
+name2 = input()  # What is their name?combined_names = name1 + name2
+combined_names = name1+name2
+lower_case = combined_names.lower()
+t = lower_case.count("t")
+r = lower_case.count("r")
+u = lower_case.count("u")
+e = lower_case.count("e")
+first_digit = t + r + u + e
+
+l = lower_case.count("l")
+o = lower_case.count("o")
+v = lower_case.count("v")
+e = lower_case.count("e")
+second_digit = l + o + v + e
+
+
+score = int(str(first_digit) + str(second_digit))
+if (score < 10) or (score > 90):
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
---
+++ day_1.py
@@ -0,0 +1,4 @@
+print("Welcone to Band Name Generator!")
+city = input("Which city did you grew up in?\n")
+pet_name = input("What is your pet name?\n")
+print(city + " " + pet_name)
